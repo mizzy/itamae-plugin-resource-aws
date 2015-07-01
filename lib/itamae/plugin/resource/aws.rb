ec5f3e4601516aea920f9318838005f0e3bd400a
Gosuke Miyashita <gosukenator@gmail.com>
2015-07-01 11:26:13 +0900

    Initial commit

---
+++ lib/itamae/plugin/resource/aws.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/aws/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module Aws
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/resource/aws/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Resource
+      module Aws
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
